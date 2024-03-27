9d3a882e54999c5a239ff2f790bad4a78740165d
Rom-T <93357374+Rom-T@users.noreply.github.com>
2024-03-27 13:45:33 -0400

    answers for lesson 6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,7 +3,10 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDb = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
+app.use(express.static('./public'));
 app.use(express.json());
 
 app.get('/hello', (req, res) => {
@@ -12,7 +15,10 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
